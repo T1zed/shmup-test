fdd1d02bfea045249b5b539725db83ca07145926
c.davitian <c.davitian@student.isartdigital.com>
2025-12-16 10:51:10 +0100

    [ADD] menuscene

--- shmup/Assets/GameManager.cs
+++ shmup/Assets/GameManager.cs
@@ -1,23 +1,42 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public int score = 0;
     public int life = 0;
     public PlayerBehavior player;
-    public TMP_Text scoreText; 
-    public TMP_Text lifeText;  
+
+    public TMP_Text scoreText;
+    public TMP_Text lifeText;
+
+    public string menuSceneName = "menuScene";
+    public string playSceneName = "PlayScene";
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 
     void Update()
     {
 
-        life = player.life;
+
+        if (player != null)
+        {
+            life = player.life;
+        }
 
         if (scoreText != null)
             scoreText.text = "Score: " + score;
 
         if (lifeText != null)
             lifeText.text = "Life: " + life;
+    }
+
+    public void playscene()
+    {
+            SceneManager.LoadScene(playSceneName);
+
     }
 }
